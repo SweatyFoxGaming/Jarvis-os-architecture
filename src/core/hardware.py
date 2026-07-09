b1d387404d514447e8c2d6b13a7a5006496c272b
SweatyFoxGaming <chadadamsn@gmail.com>
2026-07-09 22:57:32 +0000

    Update hardware.py

--- src/core/hardware.py
+++ src/core/hardware.py
@@ -1,6 +1,8 @@
 import psutil
 import platform
 import multiprocessing
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 from typing import Dict, Any
 
 class HardwareManager:
