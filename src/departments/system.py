5bf04b3d63a76ea7cd0f79492f9d6914763594fd
SweatyFoxGaming <chadadamsn@gmail.com>
2026-07-09 22:58:50 +0000

    Add logging configuration to system.py

--- src/departments/system.py
+++ src/departments/system.py
@@ -1,5 +1,7 @@
 import datetime
 import psutil
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 from src.departments.base import BaseDepartment, BaseWorker
 from src.core.models import Task, TaskStatus
 
